614e9a8f52c1f8dd15ef5c9d4c857a66cbbdb552
salmanpatrick5@gmail.com <salmanpatrick5@gmail.com>
2024-04-04 10:17:54 +0500

    Added stream options

--- YTDownloader/Services/VideoDownloader.cs
+++ YTDownloader/Services/VideoDownloader.cs
@@ -21,9 +21,13 @@ public class VideoDownloader : IVideoDownloader
        
         if (muxedStreams is not null && muxedStreams.Any())
         {
-            
-            var streamInfo = muxedStreams.GetWithHighestVideoQuality();
-            var metaData = new MetaData(video.Title, video.Author.ChannelTitle, video.Duration, video.Thumbnails, streamInfo.Url);
+            var streamOptions = new List<StreamOption>();
+            foreach(var option in muxedStreams)
+            {
+               
+                streamOptions.Add(new StreamOption(option.VideoQuality.ToString(), option.VideoResolution.ToString(), option.Size.ToString(),option.Container.ToString(),option.Url));
+            }
+            var metaData = new MetaData(video.Title, video.Author.ChannelTitle, video.Duration, video.Thumbnails, streamOptions);
             return metaData;
         }
         else
@@ -32,4 +36,6 @@ public class VideoDownloader : IVideoDownloader
         }
     }
 }
-public record MetaData(string title, string author, TimeSpan? duration, IReadOnlyList<Thumbnail> thumbnails, string downloadUrl);
+  
+public record MetaData(string title, string author, TimeSpan? duration, IReadOnlyList<Thumbnail> thumbnails, List<StreamOption> streamOptions);
+public record StreamOption(string quality, string resolution,string size,string container, string downloadUrl);
